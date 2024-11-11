58c127fec8b8cebef5508dcb7295ceed48f05d0d
Omarmoahamed <Omarh9655@gmail.com>
2024-11-11 15:01:25 +0200

    secound commit

---
+++ Test/CustomerTest (2023_11_26 06_51_10 UTC).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Domainlayer.Customer;
+using Domainlayer.DomainServices;
+namespace Test
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void CreateCustomerTest() 
+        {
+            var customer = Customer.RegisterCustomer(
+
+                Guid.NewGuid().ToString(),
+                "omar",
+                "omarh965.5@gmail.com",
+                "01100944552",
+                new CheckCustomerEmail(),
+                new Phonecompanycheck());
+
+            Assert.Equal("omar",customer.customerName );
+          
+
+        }
+    }
+}
---
+++ Test/EventTest (2023_11_26 13_04_45 UTC).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Domainlayer.Event;
+using Domainlayer.DomainServices;
+using Domainlayer.Share;
+
+namespace Test
+{
+    public class EventTest
+    {
+        [Fact]
+        public void CreateEvent() 
+        {
+            var ev = Event.createEvent(1,Guid.NewGuid().ToString(),new DateTime(2023,12,27),Guid.NewGuid().ToString());
+
+           var eq = new EventEquipment(Guid.NewGuid().ToString(),"flowers",URL.createurl("https://gghg.com"),ev.EventId,Guid.NewGuid().ToString(),Guid.NewGuid().ToString());
+
+            ev.AddEquipment(eq);
+
+            ev.RequestToRegister();
+
+            Assert.Equal(true, ev.Register);
+
+        }
+    }
+}
